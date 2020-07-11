19531e25020fd71254fc2ce3204c6035290e1ff4
Sumit Paliwal <sumit.paliwal@infor.com>
2020-07-11 10:49:36 +0530

    v1.0 - Project Setup with initial config added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// To use the bootstrap & fint-awesom feature throughout the application
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css"
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
